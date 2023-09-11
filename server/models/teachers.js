0cd3e546bd71a4417847278c66c207b7983da81d
Melven <melvenbagolaque@gmail.com>
2023-09-12 03:27:53 +0800

    feature: added teachers model

---
+++ server/models/teachers.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+    const Teacher = sequelize.define("teacher", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+        sequelize,
+        modelName: 'teacher',
+        tableName: 'teacher'
+    }
+    
+    );
+    const syncTable = async () => {
+        await Teacher.sync({ alter: true });
+      };
+      syncTable();
+    
+    return Teacher;
+  };
+  
